d6b64dd4d603c95c1c46f171f009ebb58839dd72
Samuel Le <robbo_rules1@live.com.au>
2023-11-12 12:32:06 +1100

    Setup hand state

---
+++ CardGameProject/Assets/Scripts/StateMachine/SubState/HandState.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandState : CombatBaseState
+{
+    public HandState(CombatStateMachine context, CombatStateFactory playerStateFactory, VariableScriptObject vso) : base(context, playerStateFactory, vso) { }
+
+    public override void EnterState()
+    {
+        Debug.Log("Hand State");
+    }
+    public override void UpdateState()
+    {
+        CheckSwitchState();
+    }
+
+    public override void FixedUpdateState() { }
+    public override void ExitState() { }
+    public override void CheckSwitchState()
+    {
+        // Attack state
+    }
+    public override void InitializeSubState() { }
+}
