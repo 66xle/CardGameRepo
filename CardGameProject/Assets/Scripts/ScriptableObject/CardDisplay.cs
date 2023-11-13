7b182bebf118c3c93e668c67dd6f2925d7c9a8f1
Samuel Le <robbo_rules1@live.com.au>
2023-11-13 13:23:15 +1100

    Created card scriptable object

--- CardGameProject/Assets/Scripts/ScriptableObject/CardDisplay.cs
+++ CardGameProject/Assets/Scripts/ScriptableObject/CardDisplay.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,5 +8,15 @@ public class CardDisplay : MonoBehaviour
 {
     public Card card;
 
+    public TMP_Text name;
+    public TMP_Text description;
+    public Image image;
 
+    private void Start()
+    {
+        name.text = card.name;
+        description.text = card.description;
+
+        image.sprite = card.image;
+    }
 }
