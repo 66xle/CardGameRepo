ad1cff9f4e73120d2e3fc30d77fd259191b81f53
Samuel Le <robbo_rules1@live.com.au>
2023-11-22 14:33:18 +1100

    Added health/stamina bar

--- CardGameProject/Assets/Scripts/Player.cs
+++ CardGameProject/Assets/Scripts/Player.cs
@@ -1,26 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     [Header("Stats")]
-    [SerializeField] float maxHealth = 100f; 
+    [SerializeField] float maxHealth = 100f;
     private float currentHealth;
 
     [SerializeField] float maxStamina = 5f;
-    private float currentStamina;
+    [HideInInspector] public float currentStamina;
 
+    [Header("References")]
+    public Slider healthBar;
+    public Slider staminaBar;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        currentStamina = maxStamina;
+        DisplayStats();
+    }
+    public void ConsumeStamina(float stamAmount)
+    {
+        currentStamina -= stamAmount;
+        DisplayStats();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(float damage)
     {
-        
+        currentHealth -= damage;
+        DisplayStats();
+    }
+
+    public void DisplayStats()
+    {
+        healthBar.value = currentHealth / maxHealth;
+        staminaBar.value = currentStamina / maxStamina;
     }
 }
