a52879d9ce38d186d8588bfb78f76d2241ce5607
Samuel Le <robbo_rules1@live.com.au>
2024-11-05 13:23:51 +1100

    Can now drag points around

--- CardGameProject/Assets/Scripts/MapEditor/MapEditor.cs
+++ CardGameProject/Assets/Scripts/MapEditor/MapEditor.cs
@@ -7,14 +7,27 @@ using UnityEngine.UIElements;
 public class MapEditor : MonoBehaviour
 {
     public GameObject contextMenu;
+    public LayerMask layerMask;
+
+    [Separator]
+
     public Transform pointHolder;
     public GameObject pointPrefab;
-    public LayerMask layerMask;
+
+    [Separator]
+
+    public Material seletedMat;
+    public Material defaultMat;
+
+    private GameObject seletedObject;
+    private Vector3 offSetDir;
+
+    private bool holdingDownLeftClick;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        holdingDownLeftClick = false;
     }
 
     // Update is called once per frame
@@ -30,7 +43,55 @@ public class MapEditor : MonoBehaviour
             contextMenu.transform.position = Input.mousePosition;
             contextMenu.SetActive(true);
         }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+
+                if (seletedObject != null)
+                    seletedObject.transform.GetComponent<MeshRenderer>().material = defaultMat;
+
+                if (hit.transform.CompareTag("Point"))
+                {
+                    seletedObject = hit.transform.gameObject;
+                    seletedObject.transform.GetComponent<MeshRenderer>().material = seletedMat;
+
+                    offSetDir = seletedObject.transform.position - hit.point;
+
+                    Debug.Log("Offset Direction Calculated: " + offSetDir); // Debug output
+
+
+                    holdingDownLeftClick = true;
+                }
+            }
+        }
+        
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            holdingDownLeftClick = false;
+        }
+
+        if (holdingDownLeftClick)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Vector3 newPosition = hit.point + offSetDir;
+                newPosition.y = seletedObject.transform.position.y;
+
+                // set position
+                seletedObject.transform.position = newPosition; 
+            }
+        }
     }
+
+    
 
     public void CreatePoint()
     {
