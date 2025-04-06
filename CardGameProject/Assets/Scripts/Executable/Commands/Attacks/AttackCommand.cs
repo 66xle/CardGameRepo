ccc45733ff9b2b9eb26ae5cf092348e72baf07a5
Samuel Le <robbo_rules1@live.com.au>
2025-04-06 16:46:29 +1000

    Removed target command

--- CardGameProject/Assets/Scripts/Executable/Commands/Attacks/AttackCommand.cs
+++ CardGameProject/Assets/Scripts/Executable/Commands/Attacks/AttackCommand.cs
@@ -59,8 +59,6 @@ public abstract class AttackCommand : ActionSequence
                     avatar.queueGameActions = target.queueGameActions;
                 }
             }
-
-            
         }
 
         yield return null;
