934e869d197f7097132a4ed3aa06d6f0ad683055
Samuel Le <robbo_rules1@live.com.au>
2025-04-06 17:19:49 +1000

    Created status command

---
+++ CardGameProject/Assets/Scripts/Executable/Commands/StatusEffects/StatusCommand.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class StatusCommand : ActionSequence
+{
+    public override IEnumerator Execute(Action<bool> IsConditionTrue)
+    {
+
+
+        yield return null;
+    }
+}
