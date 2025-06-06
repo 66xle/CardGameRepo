ad5c4b39ec959aee571e21c55984d0268be27678
Samuel Le <robbo_rules1@live.com.au>
2025-06-06 12:39:09 +1000

    Added fixed settings for level data

--- CardGameProject/Assets/Scripts/ScriptableObject/LevelData.cs
+++ CardGameProject/Assets/Scripts/ScriptableObject/LevelData.cs
@@ -9,5 +9,12 @@ public class LevelData : ScriptableObject
     public string LevelName;
     public GameObject Prefab;
 
+    [Separator]
+
     public bool IsFixed;
+
+
+    [ConditionalField(nameof(IsFixed), false, true)] public int RecommendLevel = 1;
+    [ConditionalField(nameof(IsFixed), false, true)] public CollectionWrapper<EnemyData> ListOfEnemies;
+    [ConditionalField(nameof(IsFixed), false, true)] [OverrideLabel("Gear Reward")] public CollectionWrapper<WeaponData> ListOfGear;
 }
