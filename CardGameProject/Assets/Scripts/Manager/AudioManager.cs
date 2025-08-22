c2eba0de3315335422b2edaecf2df5049d49fdbe
Samuel Le <robbo_rules1@live.com.au>
2025-08-22 14:22:34 +1000

    Link main menu buttons to audio manager

--- CardGameProject/Assets/Scripts/Manager/AudioManager.cs
+++ CardGameProject/Assets/Scripts/Manager/AudioManager.cs
@@ -2,9 +2,11 @@ using System.Collections.Generic;
 using MyBox;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Events;
 
-public enum SoundType
+public enum AudioType
 {
+    None,
     UIClick
 }
 
@@ -13,7 +15,7 @@ public class AudioManager : Singleton<AudioManager>
     [Foldout("UI", true)]
     public AudioData UIClick;
 
-    AudioSource audioSource;
+    private AudioSource audioSource;
 
     public void Start()
     {
@@ -22,28 +24,22 @@ public class AudioManager : Singleton<AudioManager>
         audioSource = GetComponent<AudioSource>();
     }
 
-    public void PlaySound(SoundType soundType)
+    public void PlaySound(AudioType audioType)
     {
         AudioData data = null;
 
-        if (SoundType.UIClick == soundType)
+        if (AudioType.UIClick == audioType)
         {
             data = UIClick;
         }
 
-        if (data == null) return;
+        if (data == null)
+        {
+            Debug.LogError("audioType is null");
+            return;
+        }
 
         audioSource.resource = data.audios[Random.Range(0, data.audios.Count)];
-
-        audioSource.Play();
-    }
-
-    public void PlaySoundButton(AudioData selectedData)
-    {
-        AudioData data = selectedData;
-
-        audioSource.resource = data.audios[Random.Range(0, data.audios.Count)];
-
         audioSource.Play();
     }
 }
