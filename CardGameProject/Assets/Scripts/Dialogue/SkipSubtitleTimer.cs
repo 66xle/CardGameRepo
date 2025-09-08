ec8859a9366a495265c207b02744eb6058c6bf22
Samuel Le <robbo_rules1@live.com.au>
2025-09-08 19:12:57 +1000

    Can now tap to skip dialogue and tap to continue dialogue

--- CardGameProject/Assets/Scripts/Dialogue/SkipSubtitleTimer.cs
+++ CardGameProject/Assets/Scripts/Dialogue/SkipSubtitleTimer.cs
@@ -6,33 +6,19 @@ public class SkipSubtitleTimer : MonoBehaviour
     public TextMeshProTypewriterEffect TypewriterEffect;
     public StandardDialogueUI StandardUI;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void InstantShowResponse()
     {
         Response[] responses = DialogueManager.currentConversationState.pcResponses;
         Subtitle subtitle = DialogueManager.currentConversationState.subtitle;
-
+        
         if (TypewriterEffect.isPlaying)
         {
             if (responses.Length > 0)
             {
-                StandardUI.ShowResponses(subtitle, responses, 0f);
-                return;
-            }
+                if (responses[0].destinationEntry.Title == "END") return;
 
-            StandardUI.ShowSubtitle(subtitle);
-            StandardUI.ShowContinueButton(subtitle);
+                StandardUI.OnContinueConversation();
+            }
         }
     }   
 }
