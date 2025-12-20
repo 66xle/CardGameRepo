ac80869ccb5affff8ebe28cafef4408fbc3c7ac8
Samuel Le <robbo_rules1@live.com.au>
2025-12-20 17:23:53 +1100

    Blacksmith tabs now shows respective gear

--- CardGameProject/Assets/Scripts/UI/Blacksmith/BlacksmithUI.cs
+++ CardGameProject/Assets/Scripts/UI/Blacksmith/BlacksmithUI.cs
@@ -1,17 +1,20 @@
 using MyBox;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 
 public class BlacksmithUI : MonoBehaviour
 {
     [MustBeAssigned] [SerializeField] GameObject GearIconPrefab;
-    [MustBeAssigned] [SerializeField] Transform InventoryParent;
+    [MustBeAssigned] [SerializeField] Transform AllInventoryParent;
+    [MustBeAssigned] [SerializeField] Transform WeaponInventoryParent;
+    [MustBeAssigned] [SerializeField] Transform ArmourInventoryParent;
+    [MustBeAssigned] [SerializeField] Transform AccessoriesInventoryParent;
     [MustBeAssigned] [SerializeField] GearSelectionUI GearSelectionUI;
 
     Action<GearData> _onClickSelectIcon;
     GearData _selectedGear;
-
 
     [MustBeAssigned] [SerializeField] EquipmentManager equipmentManager; // temp for testing
 
@@ -29,12 +32,43 @@ public class BlacksmithUI : MonoBehaviour
         List<GearData> weapons = new List<GearData>(GameManager.Instance.EquippedWeapons);
         weapons.Add(GameManager.Instance.MainHand);
 
+        List<GearData> armours = new List<GearData>(GameManager.Instance.EquippedArmour);
+
+
         foreach (GearData data in weapons)
         {
-            GameObject iconObj = Instantiate(GearIconPrefab, InventoryParent);
-            GearIconUI iconUI = iconObj.GetComponent<GearIconUI>();
-            iconUI.SetData(data, _onClickSelectIcon);
+            CreateGearIcon(data, AllInventoryParent);
+            CreateGearIcon(data, WeaponInventoryParent);
         }
+
+        foreach (GearData data in armours)
+        {
+            if (data == null) continue;
+
+            CreateGearIcon(data, AllInventoryParent);
+            CreateGearIcon(data, ArmourInventoryParent);
+        }
+
+        // Missing accessories tab for now
+
+        SelectTab(AllInventoryParent.gameObject);
+    }
+
+    private void CreateGearIcon(GearData data, Transform parent)
+    {
+        GameObject iconObj = Instantiate(GearIconPrefab, parent);
+        GearIconUI iconUI = iconObj.GetComponent<GearIconUI>();
+        iconUI.SetData(data, _onClickSelectIcon);
+    }
+
+    public void SelectTab(GameObject selectedTab)
+    {
+        AllInventoryParent.gameObject.SetActive(false);
+        WeaponInventoryParent.gameObject.SetActive(false);
+        ArmourInventoryParent.gameObject.SetActive(false);
+        AccessoriesInventoryParent.gameObject.SetActive(false);
+
+        selectedTab.SetActive(true);
     }
 
     public void SelectIcon(GearData data)
