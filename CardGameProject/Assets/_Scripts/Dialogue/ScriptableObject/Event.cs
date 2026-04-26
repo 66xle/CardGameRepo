daa38f190d16c3bea796f998c48cf22068a85d84
Samuel Le <robbo_rules1@live.com.au>
2026-04-26 14:15:38 +1000

    Rename folders to start with "_"

---
+++ CardGameProject/Assets/_Scripts/Dialogue/ScriptableObject/Event.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Event : ScriptableObject
+{
+    [Header("Dialogue")]
+    public List<DialogueNodeData> DialogueNodeData = new List<DialogueNodeData>();
+    public List<ChildEventData> listChildData = new List<ChildEventData>();
+    public int index = 0;
+    public string type;
+    public string guid; // Sets guid when loading in list
+    public string category;
+    public Event nextEvent;
+}
+
